ee48402fdc52c92f8c78e045a4ada301b5b09992
artcabrera <jbcabrera.910@gmail.com>
2023-06-11 00:05:08 +0800

    Cleanup

---
+++ src/App.ts
@@ -0,0 +1,11 @@
+import './assets/app.css';
+
+import { createApp, type App as VueApp } from 'vue';
+import { createPinia, type Pinia } from 'pinia';
+import App from './App.vue';
+
+const pinia: Pinia = createPinia();
+const app: VueApp<Element> = createApp(App);
+
+app.use(pinia);
+app.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+/** @type {import('vite').UserConfig} */
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  clearScreen: false,
+  server: {
+    strictPort: true
+  },
+  envPrefix: ['VITE_', 'TAURI_'],
+  build: {
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_DEBUG
+  },
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
